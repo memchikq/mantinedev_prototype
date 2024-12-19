59fd9378e98766113a8e1579cc996b17ef4baaf8
Derks <derks_derks@mail.ru>
2024-12-19 13:28:25 +0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "@mantine/core/styles.css";
-import "/node_modules/flag-icons/css/flag-icons.min.css";
 import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
