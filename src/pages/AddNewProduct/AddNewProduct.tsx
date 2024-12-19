ea5b73fd8b7d576cf6a08fe7c17b8031eb42dc38
Derks <derks_derks@mail.ru>
2024-12-19 21:16:29 +0500

    fix исправил стили

--- src/pages/AddNewProduct/AddNewProduct.tsx
+++ src/pages/AddNewProduct/AddNewProduct.tsx
@@ -1,5 +1,5 @@
 import Header from "../../Components/Headers/Header"
-import { Badge, Button, Card, Container, Flex, Group, Image, Paper, Text } from "@mantine/core"
+import { Badge, Button, Card, Container, Flex, Grid, Group, Image, Paper, Text } from "@mantine/core"
 import UserMenu from "../../Components/UserMenu/UserMenu"
 
 const AddNewProduct = () => {
@@ -11,24 +11,62 @@ const AddNewProduct = () => {
         <UserMenu />
         <Flex flex={1} direction={"column"} align={"center"} gap={"md"}>
           <Paper className="bg_img" w={"100%"} shadow="sm" radius={"md"} withBorder>
-            <Flex
-              p={"md"}
-              
-              className="header"
-              direction={"column"}
-              gap={"md"}
-             
-            >
-              <Text  size="30px" >
-                Products
-              </Text>
-              <Text size="md"  className="header-description-text">
+            <Flex p={"md"} className="header" direction={"column"} gap={"md"}>
+              <Text size="30px">Products</Text>
+              <Text size="md" className="header-description-text">
                 Use all the possibilities of our products to make your business more efficient and profitable
               </Text>
             </Flex>
           </Paper>
-          <Flex flex={1} w={"100%"} className="flex-col" wrap={"wrap"} gap={"md"}>
-            <Card w={"100%"} flex={1} shadow="sm" padding="lg" radius="md" withBorder>
+          <Grid grow>
+            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+              <Card w={"100%"} flex={1} style={{ flexShrink: 0 }} shadow="sm" padding="lg" radius="md" withBorder>
+                <Text ta={"center"} size="xl" fw={500}>
+                  Insurance
+                </Text>
+
+                <Card.Section p={10}>
+                  <Image
+                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+                    height={160}
+                    alt="Norway"
+                  />
+                </Card.Section>
+
+                <Text size="md" ta={"center"}>
+                  Try supply chain insurance with coverage up to $10,000,000
+                </Text>
+
+                <Button color="blue" fullWidth size="xl" variant="outline" mt="md" radius="md">
+                  Add service
+                </Button>
+              </Card>
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+              <Card flex={1} shadow="sm" padding="lg" style={{ flexShrink: 0 }} radius="md" withBorder>
+                <Text ta={"center"} size="xl" fw={500}>
+                  Insurance
+                </Text>
+
+                <Card.Section p={10}>
+                  <Image
+                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+                    height={160}
+                    alt="Norway"
+                  />
+                </Card.Section>
+
+                <Text size="md" ta={"center"}>
+                  Try supply chain insurance with coverage up to $10,000,000
+                </Text>
+
+                <Button color="blue" fullWidth size="xl" variant="outline" mt="md" radius="md">
+                  Add service
+                </Button>
+              </Card>
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>
+            <Card flex={1} shadow="sm" padding="lg" style={{ flexShrink: 0 }} radius="md" withBorder>
               <Text ta={"center"} size="xl" fw={500}>
                 Insurance
               </Text>
@@ -37,7 +75,7 @@ const AddNewProduct = () => {
                 <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png" height={160} alt="Norway" />
               </Card.Section>
 
-              <Text size="md" ta={"center"} >
+              <Text size="md" ta={"center"}>
                 Try supply chain insurance with coverage up to $10,000,000
               </Text>
 
@@ -45,41 +83,8 @@ const AddNewProduct = () => {
                 Add service
               </Button>
             </Card>
-            <Card flex={1} shadow="sm" padding="lg" radius="md" withBorder>
-              <Text ta={"center"} size="xl" fw={500}>
-                Insurance
-              </Text>
-
-              <Card.Section p={10}>
-                <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png" height={160} alt="Norway" />
-              </Card.Section>
-
-              <Text size="md" ta={"center"} >
-                Try supply chain insurance with coverage up to $10,000,000
-              </Text>
-
-              <Button color="blue" fullWidth size="xl" variant="outline" mt="md" radius="md">
-                Add service
-              </Button>
-            </Card>
-            <Card flex={1} shadow="sm" padding="lg" radius="md" withBorder>
-              <Text ta={"center"} size="xl" fw={500}>
-                Insurance
-              </Text>
-
-              <Card.Section p={10}>
-                <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png" height={160} alt="Norway" />
-              </Card.Section>
-
-              <Text size="md" ta={"center"} >
-                Try supply chain insurance with coverage up to $10,000,000
-              </Text>
-
-              <Button color="blue" fullWidth size="xl" variant="outline" mt="md" radius="md">
-                Add service
-              </Button>
-            </Card>
-          </Flex>
+            </Grid.Col>
+          </Grid>
         </Flex>
       </Flex>
     </Container>
